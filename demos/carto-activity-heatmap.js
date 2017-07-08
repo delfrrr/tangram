d10d1cd1c991014f76312f43ff1fe75849f9eeb5
Vova Bilonenko <bilonenko.v@gmail.com>
2017-07-08 08:13:34 +0200

    show data from carto

--- demos/carto-activity-heatmap.js
+++ demos/carto-activity-heatmap.js
@@ -1,5 +1,6 @@
 (function () {
-    const sceneUrl = 'demos/activity-heatmap.yaml';
+    const sceneUrl = 'demos/reduced-basemap.yaml';
+    const zoom = 10;
     const map = L.map('map', {
         maxZoom: 20,
         zoomSnap: 0,
@@ -7,51 +8,57 @@
         keyboard: false
     });
 
-    map.setView([52.518528, 13.405057], 10);
+    map.setView([52.518528, 13.405057], zoom);
 
     const layer = Tangram.leafletLayer({
         scene: sceneUrl,
         logLevel: 'debug'
     });
 
-    layer.scene.subscribe({
-        load: function (msg) {
-            // scene was loaded
-            injectAPIKey(msg.config);
-        },
-        update: function (msg) {
-            // scene was updated
-            injectAPIKey(msg.config);
-        },
-        view_complete: function (msg) {
-            // new set of map tiles was rendered
-        },
-        error: function (msg) {
-            // debugger;
-        },
-        warning: function (msg) {
-            // debugger;
-        }
-    });
-
-    function injectAPIKey(config) {
-        if (config.global && config.global.sdk_mapzen_api_key) {
-            config.global.sdk_mapzen_api_key = 'mapzen-T3tPjn7';
-        }
-        else {
-            for (var name in config.sources) {
-                var source = config.sources[name];
-                if (source.url.search('mapzen.com') > -1) {
-                    source.url_params = source.url_params || {};
-                    source.url_params.api_key = 'mapzen-T3tPjn7';
+    layer.scene.subscribe({load() {
+        const tileUrl = 'https://cartocdn-ashbu_b.global.ssl.fastly.net/delfrrr/api/v1/map/delfrrr@3a8feb4b@e68654b9d76308e16e7770645b74e9e7:1499180180411/1/{z}/{x}/{y}.mvt'
+        layer.scene.config.sources.carto = {
+            type: 'MVT',
+            url: tileUrl,
+            url_subdomains: ['a', 'b', 'c'],
+            transform: (function (data) {
+                const result = {
+                    heatmap: data['644146be-8e24-4b77-93b6-820cf85aa4b2']
                 }
-            }
-        }
-    }
-
+                console.log(result);
+                return result;
+            }).toString()
+        };
+        layer.scene.updateConfig();
+    }});
     window.addEventListener('load', function () {
-        console.log('layer.addTo');
         layer.addTo(map);
+        // cartodb.Tiles.getTiles({
+        //     user_name: 'delfrrr',
+        //     sublayers: [{
+        //         sql: `
+        //             SELECT
+        //             FLOOR(((ST_X(activities_small.the_geom_webmercator) + 20037508.3427892)/ 40075016.68557849) * (256 * POWER(2, ${zoom}))) as x,
+        //             FLOOR(((20037508.3427892 - ST_Y(activities_small.the_geom_webmercator))/ 40075016.68557849) * (256 * POWER(2, ${zoom}))) as y,
+        //             ST_Centroid(ST_Collect(activities_small.the_geom_webmercator)) as the_geom_webmercator,
+        //             COUNT(activities_small.cartodb_id)
+        //             FROM activities_small
+        //             GROUP BY x, y
+        //         `,
+        //         cartocss: '#layer { marker-fill: red;} '
+        //     }]
+        // }, (res, err) => {
+        //     if (err) {
+        //         throw err;
+        //     }
+        //     const tileUrl = res.tiles[0].replace(/\.png/, '.mvt');
+        //     layer.scene.config.sources.carto = {
+        //         type: 'MVT',
+        //         url: tileUrl,
+        //         url_subdomains: ['a', 'b', 'c']
+        //     };
+        //     layer.scene.updateConfig();
+        // });
     });
 
 }());
